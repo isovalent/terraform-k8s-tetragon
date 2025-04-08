2e71f284f2a897ce54976ccd37c4301096ee5fef
Philip Schmid <phisch@cisco.com>
2025-04-08 09:28:59 +0200

    Added configurable timeout for the CRD wait logic
    Signed-off-by: Philip Schmid <phisch@cisco.com>

--- scripts/tp-deployer.sh
+++ scripts/tp-deployer.sh
@@ -9,20 +9,23 @@ do
 done
 
 # Wait for the Tetragon CRDs to be available. They should be installed by the Tetragon Operator.
-TETRAGON_CRD_AVAILABILITY_COUNT=1
-set +e
-until kubectl get crd tracingpolicies.cilium.io
-do
-  if [[ ${TETRAGON_CRD_AVAILABILITY_COUNT} -gt 60 ]];
-  then
-    echo "Tetragon CRDs are not available. Check Tetragon installation."
-    exit 1
-  else
-    TETRAGON_CRD_AVAILABILITY_COUNT=$((TETRAGON_CRD_AVAILABILITY_COUNT+1))
-    sleep 1
-  fi
-done
-set -e
+if [[ "${WAIT_FOR_TETRAGON_CRDS}" == "true" ]];
+then
+  TETRAGON_CRD_AVAILABILITY_COUNT=1
+  set +e
+  until kubectl get crd tracingpolicies.cilium.io
+  do
+    if [[ ${TETRAGON_CRD_AVAILABILITY_COUNT} -gt ${WAIT_FOR_TETRAGON_CRDS_TIMEOUT} ]];
+    then
+      echo "Tetragon CRDs are not available. Check Tetragon installation."
+      exit 1
+    else
+      TETRAGON_CRD_AVAILABILITY_COUNT=$((TETRAGON_CRD_AVAILABILITY_COUNT+1))
+      sleep 1
+    fi
+  done
+  set -e
+fi
 
 # Deploy all provided TracingPolicies. Only apply *.yaml files.
 kubectl apply $(ls "${TP_DIR}"/*.yaml | awk ' { print " -f " $1 } ')
